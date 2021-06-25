41449774d527471b1bd94bab99a2afc7d56f40d0
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-06-26 01:32:13 +1000

    fetch api post and onsubmit added

--- redux_example/src/components/Postform.js
+++ redux_example/src/components/Postform.js
@@ -10,6 +10,7 @@ class PostForm extends Component {
         }
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
@@ -18,14 +19,31 @@ class PostForm extends Component {
 
     }
 
+    onSubmit(e) {
+        e.preventDefault();
 
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
 
     render() {
   
         return (
             <div>
                 <h1>Add Posts</h1>    
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label>Title: </label><br />
                         <input type='text' name='title' onChange={this.onChange} value={this.state.title} />
