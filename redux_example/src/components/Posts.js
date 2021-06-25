f20cdfc7b60b42dc9fa766c369156d2b9347fbab
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-06-25 11:50:38 +1000

    component state added to Posts

--- redux_example/src/components/Posts.js
+++ redux_example/src/components/Posts.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 
 class Posts extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state ={
+            posts: []
+        }
+    }
+
     componentWillMount() {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => this.setState({posts: data}));
     }
     render() {
         return (
