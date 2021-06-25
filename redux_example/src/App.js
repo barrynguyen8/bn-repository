0038031ab3524b39b3e080ad415396bb0d92318b
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-06-26 01:20:52 +1000

    form added

--- redux_example/src/App.js
+++ redux_example/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Posts from './components/Posts'
+import PostForm from './components/Postform'
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <PostForm />
+      <hr />
       <Posts />
     </div>
   );
--- redux_example/src/components/Posts.js
+++ redux_example/src/components/Posts.js
@@ -4,7 +4,7 @@ class Posts extends Component {
 
     constructor(props) {
         super(props);
-        this.state ={
+        this.state = {
             posts: []
         }
     }
@@ -15,10 +15,18 @@ class Posts extends Component {
             .then(data => this.setState({posts: data}));
     }
     render() {
+        const postItems = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+            
+        ));
+
         return (
             <div>
                 <h1>Posts</h1>
-                
+                {postItems}               
             </div>
         )
     }
