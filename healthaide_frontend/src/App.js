a5668792de36a40c87dd50b54e5435342afe4651
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-07-12 00:00:32 +1000

    login form added

--- healthaide_frontend/src/App.js
+++ healthaide_frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Login from './components/Login'
 import Healthaide from './components/Healthaide'
+
+const currentUser = function() {
+  const user = localStorage.getItem('user')
+  console.log(user)
+  return(user)
+}
 
 function App() {
   return (
     <div className="App">
       <h1>HealthAide</h1>
-      <Healthaide />
+      {currentUser() ? <Healthaide /> : <Login />}
     </div>
   );
 }
