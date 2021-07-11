08b3f663ca990ec49d7bde29438f958e51572384
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-07-12 00:58:02 +1000

    signup form added

--- healthaide_frontend/src/App.js
+++ healthaide_frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './components/Login'
 import Healthaide from './components/Healthaide'
 import AppHeader from './components/AppHeader'
+import Signup from './components/Signup'
 
 const currentUser = function() {
   const user = localStorage.getItem('user')
@@ -13,9 +14,11 @@ function App() {
   return (
     <div className="App">
       <AppHeader />
-      {currentUser() ? <Healthaide /> : <Login />}
+      {currentUser() ? 
+        <Healthaide /> : 
+        <><Login /> <Signup /></>}
     </div>
-  );
+  )
 }
 
 export default App;
