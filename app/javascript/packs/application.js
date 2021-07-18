66249ac3b3cd822cb740cd794c78aa4978b8cffa
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-07-19 00:34:53 +1000

    initial commit image submission added

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,29 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <script src="/packs/js/application-8ffab9e70a204f766274.js"></script> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+
+import "stylesheets/application"; // add this line
---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+var tailwindcss = require("tailwindcss"); // add this line
+
+module.exports = {
+  plugins: [
+    // add the line below
+    tailwindcss("./app/javascript/stylesheets/tailwind.config.js"),
+    
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
+}
