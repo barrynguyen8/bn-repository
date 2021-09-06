e5240d4ceec75345f928ff79137dba7ab96fe77e
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-09-07 01:04:23 +1000

    revised index.js

--- restaurant-reviews/backend/index.js
+++ restaurant-reviews/backend/index.js
@@ -1,23 +1,25 @@
-import app from "./server.js" 
-import mongodb from "mongodb" 
+import app from "./server.js";
+import mongodb from "mongodb"
 import dotenv from "dotenv"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8000
 
-MongoClient.connect( //connect to db 
+MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
     {
-        poolSize: 50,
-        wtimeout: 2500,
-        useNewUrlParse: true }
-    )
-    .catch(err => {
-        console.log(err.stack)
-        process.exit(1)
+        maxPoolSize:50,
+        wtimeoutMS:2500,
+        useNewUrlParser:true
+    }
+)
+.catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+})
+.then(async client => {
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
     })
-    .then(async client => {
-        app.listen() //start web server 
-    })
-
+})
