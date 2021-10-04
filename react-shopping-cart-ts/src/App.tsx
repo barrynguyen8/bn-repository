3f345a3cccb228fc9bb92eb6d94fa5a62d0fec27
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-10-05 00:06:44 +1100

    test react-query

--- react-shopping-cart-ts/src/App.tsx
+++ react-shopping-cart-ts/src/App.tsx
@@ -10,7 +10,7 @@ import Badge from '@material-ui/core/Badge';
 import { Wrapper } from './App.styles';
 // Types
 export type CartItemType = {
-  id:number;
+  id: number;
   category: string;
   description: string;
   image: string;
@@ -19,12 +19,12 @@ export type CartItemType = {
   amount: number; 
 }
 
-
-
-const getProducts = async (): Promise<CartItemType> =>
-  await (await fetch('https://fakestoreapi.com/prodcuts')).json(); 
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json(); 
 
 const App = () => {
+  const { data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
   return (
     <div className="App">
       Start
